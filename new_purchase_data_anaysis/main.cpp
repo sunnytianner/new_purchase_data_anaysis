e860cf05ab4e4c557140aff3c27c238427366301
Eagle_Han <tian19880220@126.com>
2013-07-24 08:43:08 +0800

    Initial Commit

---
+++ new_purchase_data_anaysis/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  new_purchase_data_anaysis
+//
+//  Created by Eagle on 13-7-24.
+//  Copyright (c) 2013年 Eagle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
